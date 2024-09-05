718cba1789984781cf3d4409898845068baf8d0e
Marc Morcos <marc.morcos1@hotmail.com>
2024-09-05 16:10:36 -0400

    upload code and outputs

---
+++ databaseWordCounter.py
@@ -0,0 +1,181 @@
+from concurrent.futures import ThreadPoolExecutor
+import re
+import csv
+import os
+import pandas as pd
+import numpy as np
+import time
+import traceback
+from tqdm import tqdm
+import json
+
+#get list of words
+def getWords(text):
+    #remove everything but words, digits, whitespace, apostrophe, and dash (replace with space)
+    text = re.sub(r'[^\w\d\s\'-]+', ' ', text)
+    #lowercase
+    text = text.lower()
+    #split into words (removes whitespace)
+    text=text.split()
+    #get rid of 's at END of words
+    text = [re.sub("'s$", '', word) for word in text]
+
+    return text
+
+#get brief_summary and detailed_description
+def getSectionsWeWant(filepath):  
+    with open(filepath, "r", encoding="utf8") as f:
+        data = json.load(f)
+        description = data["protocolSection"]["descriptionModule"]
+        text=description.get('briefSummary', "") + "\n" + data.get('detailedDescription', "")    
+        assert(text.strip()!="")
+    
+    return text
+    
+
+#what each thread will do
+#get a nice word list of the sections we want from an json file
+def preProcessItem(inputItem):
+        try:
+            filepath = os.path.join(os.getcwd(),"ctg-studies",inputItem["NCTNum"]+".json")
+            text = getSectionsWeWant(filepath)
+            words = getWords(text)
+            inputItem["words"] = words
+            return inputItem
+        except Exception as e:
+            print("error while processing")
+            print(inputItem)
+            traceback.print_exc()
+            raise e
+        
+
+def main():
+    startTime = time.time()
+    print("Starting")
+
+    wordsWeWant = "Depression,anxiety,stress,distress" #None
+    
+    if(wordsWeWant is not None):
+        wordsWeWant = getWords(wordsWeWant)
+        print("Edited wordswewant list to look like the following:",wordsWeWant)
+        wordsWeWant = [wordInList.lower() for wordInList in wordsWeWant] #lowercase
+
+    #change directory to current file path
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+
+    #check for old output and create output folder
+    outputDir = os.path.abspath("./Output")
+    os.makedirs(outputDir, exist_ok = False)
+
+    #read original csv
+    statuses = dict()
+    toGet = []
+    inputFile = os.path.abspath("./ctg-studies.csv")
+    inputDF = pd.read_csv(inputFile, engine='python').dropna(how="all")
+    for index, row in inputDF.iterrows():
+        yearKeyToLookFor = "Start Date"
+        year = row[yearKeyToLookFor]
+        if(type(year) != str):
+            year = "No "+yearKeyToLookFor
+        else:
+            year=year[:4]
+
+        status = row["Study Status"].upper()
+        NCTNum = row["NCT Number"]
+
+        #prepare output
+        if status not in statuses:
+            statuses[status] = dict()
+        if year not in statuses[status]:
+            statuses[status][year] = [dict(),0] #[word dict, number of studies for that year]
+        
+        #prepare input
+        toGet.append({"year":year,
+                       "status":status,
+                       "NCTNum":NCTNum})
+
+
+    print("Starting file reads, total current runtime:",time.time()-startTime)
+    #read all the json files and get word lists
+    numThreads = 4
+    with ThreadPoolExecutor(numThreads) as executor:
+        toWaitFor = []
+        for item in toGet:
+            toWaitFor.append(executor.submit(preProcessItem,item))
+        
+        results = []
+        for item in tqdm(toWaitFor,desc= "Reading files and doing some preprocessing", leave=True):
+            results.append(item.result())
+    
+    print("Starting Finalization:",time.time()-startTime)
+
+    #count statistics
+    for study in results:
+        yearArr = statuses[study["status"]][study["year"]]
+        yearDict = yearArr[0]
+        yearArr[1] = yearArr[1]+1
+        wordsFoundInCurrentStudy = set()
+        currentWordlist = study["words"]
+        for word in currentWordlist:
+            #filter only words we want
+            if wordsWeWant is not None and (word not in wordsWeWant):
+                continue
+
+            #word already found in year
+            if word in yearDict:
+                yearDict[word][0] = yearDict[word][0]+1
+            else:
+                yearDict[word] = [1,[],0] #[word count, list of percent of word in respective text,num studies in year mentioning word]
+            
+            #word not already found in block
+            if(word not in wordsFoundInCurrentStudy):
+                #percent of word in respective text
+                wordsFoundInCurrentStudy.add(word)
+                percentOfWord = 100.0*currentWordlist.count(word)/len(currentWordlist)
+                yearDict[word][1].append(percentOfWord)
+                #count num studies in year mentioning word
+                yearDict[word][2]+=1
+    
+    
+    #format data
+    colTitles = ["word","number of mentions","Avg percent of mentions per study","percent of studies in year mentioning word"]
+    colsPerYear = len(colTitles)
+    for statusKey,statusVal in statuses.items():
+
+        #count max words
+        maxWords = 0
+        for yearVal in statusVal.values():
+            maxWords = max(maxWords,len(yearVal[0]))
+        
+        shape = (maxWords+2,colsPerYear*len(statusVal))
+        output =np.full(shape, "", dtype="object", order='C')
+
+        sortedYears = sorted(statusVal.items(), key=lambda item: int(item[0]) if item[0].isdecimal() else 99999999)
+        for yearInd,(yearKey,yearValArr) in enumerate(sortedYears):
+            yearVal = yearValArr[0]
+            #titles
+            col = yearInd*colsPerYear
+            output[0,col] = yearKey
+            output[0,col+1] = "Num studies:"+str(yearValArr[1])
+            output[1,col:col+colsPerYear]=colTitles
+
+            #values
+            sortedWords = sorted(yearVal.items(), key=lambda item: item[1][0],reverse =True)
+            for row,(wordKey,wordVal) in enumerate(sortedWords):
+                #for average, divide by number of studies in group (not len(wordVal[1]) since wordVal[1] has no entries of 0%)
+                output[row+2,col:col+colsPerYear]=[wordKey,wordVal[0],str(sum(wordVal[1])/yearValArr[1])+"%",str(100*wordVal[2]/yearValArr[1])+"%"]
+    
+        #save the excel sheet with name of status
+        with open(os.path.join(outputDir,statusKey+'.csv'), 'w', newline='', encoding="utf-8-sig") as fp:
+                writer = csv.writer(fp, quoting=csv.QUOTE_NONNUMERIC)
+                writer.writerows(output.tolist())
+    
+    print("Done:",time.time()-startTime)
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ otherDataFromStudies.py
@@ -0,0 +1,167 @@
+import os
+import pandas as pd
+import numpy as np
+import json
+
+# extract attribute where we expect a single answer (or 0)
+def getSingularAttribute(attribute,root):
+    return root.get(attribute,"Not Specified")
+
+#count value in category
+def countValue(category,value, outputDict):
+    if not (category in outputDict):
+        outputDict[category] = dict()
+
+    if(value in outputDict[category]):
+        outputDict[category][value]+=1
+    else:
+        outputDict[category][value]=1
+
+
+def main():
+
+    #change directory to current file path
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+
+    #read original csv 
+    toGet = []
+    statusesWeWant = ["RECRUITING","ENROLLING_BY_INVITATION","ACTIVE_NOT_RECRUITING","COMPLETED"]
+    statusesExcluded = []
+    inputFile = os.path.abspath("./ctg-studies.csv")
+    inputDF = pd.read_csv(inputFile, engine='python').dropna(how="all")
+    for index, row in inputDF.iterrows():
+        yearKeyToLookFor = "Start Date"
+        year = row[yearKeyToLookFor]
+        if(type(year) != str):
+            continue #filter out studies with no start date
+        else:
+            
+            year=year[:4]
+            assert(year.isnumeric())
+            if(int(year)<2020):
+                year = "before2020"
+            else:
+                year = "2020AndBeyond"
+
+        status = row["Study Status"].upper()
+        NCTNum = row["NCT Number"]
+
+        #filter statuses we dont want
+        if status not in statusesWeWant:
+            if status not in statusesExcluded:
+                statusesExcluded.append(status)
+            continue
+        
+        #prepare input
+        toGet.append({"year":year,
+                       "status":status,
+                       "NCTNum":NCTNum})
+
+
+    results = {
+        "before2020": {
+            "enrollmentNotIncludingNonSpecified": []
+            },
+        "2020AndBeyond": {
+            "enrollmentNotIncludingNonSpecified": []
+            }
+    }
+    #read all the json files and get stats
+    for item in toGet:
+        filepath = os.path.join(os.getcwd(),"ctg-studies",item["NCTNum"]+".json")
+        with open(filepath, "r", encoding="utf8") as f:
+            jsonData = json.load(f)
+
+        #location country
+        countries = jsonData["protocolSection"].get("contactsLocationsModule",dict()).get("locations",[])
+        outputCountry = "Not Specified"
+        NumUS = 0
+        NumNonUS = 0
+        countriesList = []
+        for location in countries:
+                tmp = location.get("country",None)
+                if(tmp is not None):
+                    countriesList.append(tmp)
+        if(len(countries) > 0):
+            for finalCountry in countriesList:
+                if(finalCountry.lower() == "united states"):
+                    NumUS+=1
+                else:
+                    NumNonUS+=1
+            if(NumUS>0):
+                if(NumNonUS>0):
+                    outputCountry = "USA+Other"
+                else:
+                    outputCountry = "USA"
+            else:
+                outputCountry = "Other"     
+        countValue("Country",outputCountry,results[item["year"]])     
+        
+        #gender 
+        eligibility = jsonData["protocolSection"]["eligibilityModule"]
+        sex = getSingularAttribute('sex',eligibility)
+        countValue("sex",sex,results[item["year"]])   
+
+        #enrollment 
+        enrollment = getSingularAttribute("count",jsonData["protocolSection"]["designModule"].get("enrollmentInfo",dict()))
+        if(enrollment!="Not Specified"):
+            assert(isinstance(enrollment,int) or enrollment.isnumeric())
+            enrollment = int(enrollment)
+            results[item["year"]]["enrollmentNotIncludingNonSpecified"].append(enrollment)
+
+        
+        study_design_info = (jsonData["protocolSection"]["designModule"]).get("designInfo",None)
+        if(study_design_info is not None):
+
+            #allocation
+            allocation = getSingularAttribute("allocation",study_design_info)
+            countValue("allocation",allocation,results[item["year"]])        
+
+            #intervention_model
+            intervention_model=  getSingularAttribute("interventionModel",study_design_info)
+            countValue("interventionModel",intervention_model,results[item["year"]])
+
+            #masking
+            masking=  getSingularAttribute("masking",study_design_info.get("maskingInfo",dict()))
+            #get first word
+            if(masking != "Not Specified"):
+                masking = masking.split()[0]
+            countValue("masking",masking,results[item["year"]])
+
+            #primary_purpose
+            primary_purpose=  getSingularAttribute("primaryPurpose",study_design_info)
+            countValue("primaryPurpose",primary_purpose,results[item["year"]])
+        else:
+            countValue("allocation","Not Specified",results[item["year"]])        
+            countValue("interventionModel","Not Specified",results[item["year"]])
+            countValue("masking","Not Specified",results[item["year"]])
+            countValue("primaryPurpose","Not Specified",results[item["year"]])
+
+    #tally enrollment stats
+    for key in results:
+        stats = {
+            "MEAN":np.mean(results[key]["enrollmentNotIncludingNonSpecified"]),
+            "STDDEV":np.std(results[key]["enrollmentNotIncludingNonSpecified"])
+        }
+        results[key]["enrollmentNotIncludingNonSpecified"] = stats
+    
+    outputDir = os.path.abspath("./Output")
+    os.makedirs(outputDir, exist_ok = True)
+    with open(os.path.join(outputDir,"otherData.txt"), 'w') as f:
+        f.writelines([\
+        "_Results_",\
+        "\nTotal Studies: "+str(len(inputDF)),\
+        "\nSections we want: "+str(statusesWeWant),\
+        "\nNum studies after applying filters: "+str(len(toGet)),\
+        "\nSections we dont want: "+str(statusesExcluded),\
+        "\n"+json.dumps(results, indent=4),\
+        ])
+
+
+if __name__ == "__main__":
+    main()
+
+
+
